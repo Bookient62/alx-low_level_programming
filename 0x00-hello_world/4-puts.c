4f34c0c43bdaa4205959d59c522477a1a61e5c22
Bookient62 <Josephboluwatife62@gmail.com>
2022-06-02 15:00:25 -0700

    my puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,8 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: prints "Programming is like building a multilingual puzzle"
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Return:0
  */
 int main(void)
 {
