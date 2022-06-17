86baff8eb5f9606160f83ef9ae25dbd1b1fc57ac
Bookient62 <Josephboluwatife62@gmail.com>
2022-06-17 08:21:38 -0700

    hot fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,15 @@
 #include "main.h"
+
+
 /**
- * _strcat -> this function for more point and array
- * @dest: first param
- * @src: second param
- * Return: a string
- */
+* _strcat -> this is a funcion strcat
+* @dest: first param
+* @src: second param
+* Return: a string
+*/
 char *_strcat(char *dest, char *src)
 {
-	int len = 0, 1;
+	int len = 0, i;
 
 	while (dest[len])
 		len++;
